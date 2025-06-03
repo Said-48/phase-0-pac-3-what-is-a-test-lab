6a46dc5b12f12aff96b3af74a06297475ca57fb1
saidabdulhakim48@gmail.com <saidabdulhakim48@gmail.com>
2025-06-04 00:21:01 +0300

    Completed Lab

--- test/indexTest.js
+++ test/indexTest.js
@@ -11,8 +11,8 @@ describe("what-is-a-test", () => {
 
   describe("Height", () => {
     it("is less than 40 and greater than 0", () => {
-      expect(height).toBeMoreThan(0)
-      expect(height).toBeLessThan(40);
+      expect(height).toBeMoreThan(73)
+      expect(height).toBeLessThan(75);
     });
   });
 
